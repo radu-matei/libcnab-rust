e414aba09657493a07a9bb837a0469bf950f1f99
Radu Matei <radu@radu-matei.com>
2019-03-23 06:50:29 +0200

    Update library to use canonical_json

--- src/cnab.rs
+++ src/cnab.rs
@@ -1,14 +1,18 @@
+use canonical_json::error::Error::Syntax;
+use canonical_json::*;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
 /// Bundle implements a CNAB bundle descriptor
 ///
 /// Bundle descriptors describe the properties of a bundle, including which images
 /// are associated, what parameters and credentials are configurable, and whether there
-/// are any additional traget actions that can be executed on this bundle.
+/// are any additional target actions that can be executed on this bundle.
 ///
 /// The fields here are in canonical order.
 #[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
+// #[serde(rename_all = "camelCase")]
+// TODO - Radu M - fix camel case naming for fields
 pub struct Bundle {
     /// The list of additional actions that this bundle can perform.
     ///
@@ -31,9 +35,10 @@ pub struct Bundle {
     /// Each image here is considered a constituent of the application described by this
     /// bundle.
     pub images: Option<HashMap<String, Image>>,
-    /// inovcation_images is the list of available bootstrapping images for this bundle
+    /// invocation_images is the list of available bootstrapping images for this bundle
     ///
     /// Only one ought to be executed.
+    #[serde(rename = "invocationImages")]
     pub invocation_images: Vec<Image>,
     /// keywords is a list of keywords describing this bundle
     pub keywords: Option<Vec<String>>,
@@ -48,6 +53,7 @@ pub struct Bundle {
     /// Parameters can be injected into a bundle during startup time.
     pub parameters: Option<HashMap<String, Parameter>>,
     /// schema_version is the version of the CNAB specification used to describe this
+    #[serde(rename = "schemaVersion")]
     pub schema_version: String,
     /// version is the version of the bundle
     pub version: String,
@@ -55,14 +61,45 @@ pub struct Bundle {
 
 /// Represents a bundle.
 impl Bundle {
-    ///fn new(name: String, version: String) -> Bundle {}
-    pub fn from_string(json_data: &str) -> Result<Bundle, serde_json::Error> {
-        let res: Bundle = serde_json::from_str(json_data)?;
-        Ok(res)
+    // TODO - Radu M - this returns canonical_json::Error
+    // consider returning either serde_json::Error, or a plain error
+    pub fn from_string(json_data: &str) -> Result<Bundle, Error> {
+        let bundle: Result<Bundle, Error> = canonical_json::from_str(json_data);
+        match bundle {
+            Ok(b) => Ok(b),
+            Err(err) => {
+                match err {
+                    // the canonical JSON parser errors out if the input JSON is not canonical
+                    // for now, we accept non-canonical JSON as input
+                    // TODO - Radu M - check other error types generated because JSON is not canonical
+                    Error::Syntax(SyntaxError::UnexpectedWhitespace, _, _) => {
+                        println!("UNEXPECTED WHITESPACE!");
+                        let b: Result<Bundle, serde_json::Error> = serde_json::from_str(json_data);
+                        match b {
+                            Ok(b) => return Ok(b),
+                            Err(err) => return Err(Error::Syntax(
+                                SyntaxError::Custom("cannot deserialize".to_string()),
+                                0,
+                                0,
+                            )),
+                        };
+                    }
+                    _ => (),
+                }
+                Err(err)
+            }
+        }
+    }
+
+    // TODO - Radu M - this returns canonical_json::Error
+    // consider returning either serde_json::Error, or a plain error
+    pub fn to_string(&self) -> Result<String, Error> {
+        let s: String = to_string(&self)?;
+        Ok(s)
     }
 }
 
-/// Maintainer describes a bundle mainainer.
+/// Maintainer describes a bundle maintainer.
 ///
 /// The name field is required, though the format of its value is unspecified.
 #[derive(Debug, Serialize, Deserialize)]
@@ -79,15 +116,19 @@ pub struct Maintainer {
 ///
 /// Both invocation images and regular images can be described using this object.
 #[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
+// #[serde(rename_all = "camelCase")]
+// TODO - Radu M - fix camel case naming for fields
+
 pub struct Image {
     /// A digest to be used to verify the integrity of the image
     pub digest: Option<String>,
     /// The image, as a string of the form REPO/NAME:TAG@SHA
     pub image: String,
     /// The type of image. Typically, this is treated as an OCI Image
+    #[serde(rename = "imageType")]
     pub image_type: Option<String>,
     /// The media type of the image
+    #[serde(rename = "mediaType")]
     pub media_type: Option<String>,
     /// The platform this image may be deployed on
     pub platform: Option<Platform>,
@@ -121,17 +162,21 @@ pub struct Credential {
 
 /// Parameter describes a parameter that will be put into the invocation image
 ///
-/// Paramters are injected into the invocation image at startup time
+/// Parameters are injected into the invocation image at startup time
 #[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
+// #[serde(rename_all = "camelCase")]
+// TODO - Radu M - fix camel case naming for fields
+
 pub struct Parameter {
     /// The actions to which this parameter applies.
     ///
     /// If unset, this parameter will be applied to all actions.
+    #[serde(rename = "applyTo")]
     pub apply_to: Option<Vec<String>>,
     /// The location where this parameter will be injected in the invocation image
     pub destination: Destination,
     /// This parameter's default value
+    #[serde(rename = "defaultValue")]
     pub default_value: Option<serde_json::Value>,
 
     /// An enumeration of allowed values
@@ -141,18 +186,21 @@ pub struct Parameter {
     /// The exclusive maximum.
     ///
     /// If unspecified, no exclusive max is applied
+    #[serde(rename = "exclusiveMaximum")]
     pub exclusive_maximum: Option<i64>,
     /// The exclusive minimum.
     ///
     /// If unspecified, no exclusive min is applied
+    #[serde(rename = "exclusiveMinimum")]
     pub exclusive_minimum: Option<i64>,
     /// The maximum
     ///
-    /// If unspecieid, the maximum 64-bit integer value is applied
+    /// If unspecified, the maximum 64-bit integer value is applied
     pub maximum: Option<i64>,
     /// The maximum length of a string value
     ///
     /// If unspecified, no max is applied.
+    #[serde(rename = "maxLength")]
     pub max_length: Option<i64>,
     /// Additional parameter information
     pub metadata: Option<Metadata>,
@@ -161,6 +209,7 @@ pub struct Parameter {
     /// If unspecified, the minimum 64-bit integer value is applied
     pub minimum: Option<i64>,
     /// The minimum string length
+    #[serde(rename = "minLength")]
     pub min_length: Option<i64>,
     /// A regular expression (as defined in ECMAScript)
     ///
@@ -190,7 +239,7 @@ pub struct Action {
     /// If true, this action does not require any state information to be injected
     ///
     /// For example, printing help text does not require an installation, credentials,
-    /// or paramters.
+    /// or parameters.
     #[serde(default)]
     pub stateless: bool,
 }
@@ -202,12 +251,12 @@ pub struct Metadata {
     pub description: Option<String>,
 }
 
-/// Destination describes where, in the invocation image, a particular paramter value should be
+/// Destination describes where, in the invocation image, a particular parameter value should be
 /// placed.
 ///
 /// A parameter value can be placed into an environment variable (`env`) or a file at
-/// a particular location on the filesystem (`path`). This is a non-exclusive or, meaining
-/// that the same paramter can be written to both an env var and a path.
+/// a particular location on the filesystem (`path`). This is a non-exclusive or, meaning
+/// that the same parameter can be written to both an env var and a path.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Destination {
     /// The name of the destination environment variable
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
+#[macro_use]
 extern crate serde;
 #[macro_use]
-extern crate serde_derive;
 extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate canonical_json;
+extern crate spectral;
 
 pub mod cnab;
 
